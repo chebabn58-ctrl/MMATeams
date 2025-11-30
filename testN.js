5561a504e95df31dd674ff82d03eafeda1c30d20
chebabn58-ctrl <chebabn58@gmail.com>
2025-11-30 13:22:50 -0800

    Implement addition and multiplication functions
    Added functions for addition and multiplication with test cases.

--- testN.js
+++ testN.js
@@ -1,1 +1,13 @@
-console.log("Test SonarCloud QA");
+// Petit test SonarCloud - QA
+
+function addition(a, b) {
+    return a + b;
+}
+
+function multiplication(a, b) {
+    return a * b;
+}
+
+// Test des fonctions
+console.log("Addition 2 + 3 =", addition(2, 3));
+console.log("Multiplication 4 * 5 =", multiplication(4, 5));
